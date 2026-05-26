1e03ee5fd304504545e2e82a87f6ffcb122a11fb
Juan Ignacio Llufriu <142595547+juanillufriu@users.noreply.github.com>
2026-05-26 15:37:28 -0300

    Updates DQL PostgreSQL files

--- DQL-pastas.sql
+++ DQL-pastas.sql
@@ -1,5 +1,5 @@
 -- Precio de Venta, Costo y Ganancia de Productos Rellenos
-SELECT nombre, precio_venta from producto
+SELECT nombre, precio_venta, costo_unitario.ingrediente,(precio_venta.producto - (cantidad.receta_detalle*costo_unitario.ingrediente) AS Ganancia) from producto
 JOIN tipo_producto ON producto.id_tipo = tipo_producto.id_tipo
 JOIN receta_detalle ON receta_detalle.id_producto = producto.id_producto
 JOIN ingrediente ON receta_detalle.id_ingrediente = ingrediente.id_ingrediente
